32339c5aaaea6a3f7443c0a8258d8b881a5d1203
flirtman <iliasvinin@gmail.com>
2020-03-24 20:02:22 -0400

    header test recatoring

--- src/__tests__/components/header/index.test.js
+++ src/__tests__/components/header/index.test.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../../components/header/index';
 
-
+const setUp = (props={}) => {
+    const component = shallow(<Header {...props} />);
+    return component;
+};
 
 describe('Header Component', () => {
+
+    let component;
+    beforeEach(() => {
+        component = setUp();
+    });
+
     it('It should render without errors', () => {
-        const component = shallow(<Header/>);
         const wrapper = component.find('.headerComponent');
         expect(wrapper.length).toBe(1);
     });
 
     it('It should render a logo', () => {
-        const component = shallow(<Header/>);
         const logo = component.find('.logo-adidas');
         expect(logo.length).toBe(1);
     });
